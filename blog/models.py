f731682485402ae572deb8466f025ed57bc16225
Samuel Näsström <nasstromsamuel@icloud.com>
2022-12-01 14:19:45 +0000

    Changed the Post model

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,8 @@ app_name = 'blog'
 
 
 class Post(models.Model):
-    title: models.CharField(max_length=200, unique=True)
+    title = models.CharField(
+        max_length=200, unique=True, default='placeholder')
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='blog_posts')
