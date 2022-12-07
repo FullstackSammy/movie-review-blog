a5b00a236fd5331ffe9fb1cae0d3505563a6039e
Samuel Näsström <nasstromsamuel@icloud.com>
2022-12-07 00:28:44 +0000

    Finished form page and functions

--- static/js/script.js
+++ static/js/script.js
@@ -11,9 +11,9 @@ document.addEventListener("DOMContentLoaded", function () {
         if (email === "") {
             alert('Please type a username and email');
         } else {
+            alert('Email sent with suggestions. Hope you have a great movie night!');
             emailjs.send("service_ie8ap9o", "template_xcpz6ol", templateParams, "R6A6UlPYHLb2KXBMs").
             then(function () {
-                alert('Email with score was sent!');
                 document.getElementById("exampleFormControlInput1").value = "";
             }, function (error) {
                 alert('FAILED...', error);
